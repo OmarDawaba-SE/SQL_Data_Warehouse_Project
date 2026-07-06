f22ac376dea641b28554e054fc115a2ae3546a01
OmarDawaba-SE <omardawaba99@gmail.com>
2026-07-06 17:18:37 +0300

    Update bronze_load_data.sql

--- scripts/bronze_load_data.sql
+++ scripts/bronze_load_data.sql
@@ -1,5 +1,6 @@
 --loading data from csv into tables 
 --NOTE: replace YourDataPath in FROM caluse with your csv path
+TRUNCATE TABLE bronze.crm_cust_info; --scince committing full load, making sure tables are empty before loading data (avoiding duplicates) 
 BULK INSERT bronze.crm_cust_info
 FROM 'YourDataPath\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 WITH(
@@ -8,6 +9,7 @@ WITH(
 	,TABLOCK --so it locks the csv file while loading data
 );
 
+TRUNCATE TABLE bronze.crm_prd_info;
 BULK INSERT bronze.crm_prd_info
 FROM 'YourDataPath\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 WITH(
@@ -16,6 +18,7 @@ WITH(
 	,TABLOCK --so it locks the csv file while loading data
 );
 
+TRUNCATE TABLE bronze.crm_sales_details;
 BULK INSERT bronze.crm_sales_details
 FROM 'YourDataPath\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 WITH(
@@ -24,6 +27,7 @@ WITH(
 	,TABLOCK --so it locks the csv file while loading data
 );
 
+TRUNCATE TABLE bronze.erp_cust_az12;
 BULK INSERT bronze.erp_cust_az12
 FROM 'YourDataPath\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 WITH(
@@ -32,6 +36,7 @@ WITH(
 	,TABLOCK --so it locks the csv file while loading data
 );
 
+TRUNCATE TABLE bronze.erp_loc_a101;
 BULK INSERT bronze.erp_loc_a101
 FROM 'YourDataPath\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 WITH(
@@ -40,6 +45,7 @@ WITH(
 	,TABLOCK --so it locks the csv file while loading data
 );
 
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 BULK INSERT bronze.erp_px_cat_g1v2
 FROM 'YourDataPath\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 WITH(
